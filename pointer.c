06414518499ed66d3f6996da9c90e0b4081c5cc2
Gowthamrec <gowthamcr66@gmail.com>
2026-02-25 00:05:19 +0530

    next

--- pointer.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int x =10;
-    int *ptr = &x; 
-    printf("Value of x: %d\n", x); 
-    *ptr = 20;
-    printf("Value of x after modifying through pointer: %d\n", x); // still prints 10, ptr is now pointing to a different value
-}
